12c36a50fb1dddecf3976dfcf22b421c553a1b88
skybase986-a11y <skybase986@gmail.com>
2026-02-09 15:38:18 -0800

    Update game.js

--- Grid-Platformer/game.js
+++ Grid-Platformer/game.js
@@ -539,15 +539,11 @@ window.loadLevel = function(compressedData, scene = window.myGameScene) {
 
 
     // SET MUSIC 
-// SET MUSIC 
-if (levelData.music && gameState !== 'title' && player.body?.enabled === true) {
-    if (this.currentMusic) {
-        this.currentMusic.stop();
-    }
+if (levelData.music && gameState === 'playing') {  // Only if in gameplay
+    if (this.currentMusic) this.currentMusic.stop();
     this.currentMusic = this.sound.add(levelData.music);
     this.currentMusic.play({ loop: true });
 }
-
 
 
 
@@ -2520,8 +2516,6 @@ function openBackgroundMenu() {
   // Add background selection UI here if needed
   showInstruction(this, 'Background menu coming soon!', 2000);
 }
-
-
 
 
 
